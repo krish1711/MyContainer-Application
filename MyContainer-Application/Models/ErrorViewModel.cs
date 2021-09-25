e70297054bee0c8872e1d39e74554351084c6091
kishore <kishore@Dev-VM>
2021-09-25 05:15:03 +0000

    Add project files.

---
+++ MyContainer-Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyContainer_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
